5c206bd73cb39f500d1770207c8bbc72cbe60ca1
DamarisCaballero <113944199+DamarisCaballero@users.noreply.github.com>
2024-12-01 18:57:06 -0600

    changes
    final

---
+++ portafolio/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+    return (
+    <section className="about">
+    <h2>Sobre Mí</h2>
+    <p>
+    Soy alguien apasionado por resolver problemas a través de la tecnología. Me encanta trabajar en el desarrollo backend, especialmente con PHP y Python, y tengo experiencia manejando bases de datos como MySQL y MariaDB.
+Para mí, es importante construir sistemas que no solo funcionen, sino que sean eficientes, escalables y estén diseñados con una arquitectura sólida. Siempre busco aprender algo nuevo y mantenerme al día con las tendencias tecnológicas.
+    </p>
+    <p>
+    Disfruto colaborar en proyectos que marcan la diferencia, porque creo que la tecnología puede ser una herramienta poderosa para crear impacto positivo. Cada línea de código que escribo la veo como una oportunidad para mejorar y aportar algo valioso.
+    </p>
+    </section>
+    );
+    }
+    
+    export default About;
